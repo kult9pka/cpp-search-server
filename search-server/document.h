a11e6bbc653a94d86c91c8bba3af5188540d1340
Nikita Korolik <kult9pka@gmail.com>
2022-03-26 19:23:14 +1000

    Fourth sprint

---
+++ search-server/document.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+struct Document {
+    Document() = default;
+    Document(int id, double relevance, int rating);        
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
+
+std::ostream& operator<<(std::ostream& out, const Document& document);
